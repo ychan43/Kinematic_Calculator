3efe367cf3287867c6bd88661b20a5f214cec7e0
Keyur Patel <kpate325.edu>
2018-12-09 15:40:48 -0600

    evrything Done except for scenerios when under one sec

--- app/src/androidTest/java/com/example/alexc/kinematic_calculator/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/alexc/kinematic_calculator/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.kpate325.kinematic_calculator", appContext.getPackageName());
+        assertEquals("com.example.alexc.kinematic_calculator", appContext.getPackageName());
     }
 }
